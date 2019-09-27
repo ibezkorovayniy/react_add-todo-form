ed6d82b381beb623e33388ebd547bcd75c7ecffd
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-09-27 14:42:18 +0300

    Updates

---
+++ precache-manifest.8155594bd152b8ea78cbca369570fa08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203484d0ad16d45c8aa339e2e0433460",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "21c881656094e533642a",
+    "url": "/react_add-todo-form/static/css/main.6c40f2ad.chunk.css"
+  },
+  {
+    "revision": "f37a6617dcfb5646f53c",
+    "url": "/react_add-todo-form/static/js/2.58d23bf0.chunk.js"
+  },
+  {
+    "revision": "21c881656094e533642a",
+    "url": "/react_add-todo-form/static/js/main.358ab2d9.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
